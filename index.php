1e8f940f2507201a91036dcb2efd18fe8151615a
raissa-pedroso <raissa.2021319265@aluno.iffar.edu.br>
2023-11-10 08:30:44 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
-<?php
-    // tela de login
-    // mandar para model : ../model/login.php;
-
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Login</title>
+</head>
+<body>
+<section class="body">
+    <div class="container">
+        <div class="login-box">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="logo">
+                        <span class="logo-font">Controle </span>de Vendas
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <br>
+                    <h2 class="header-title">Login</h2>
+                    <form class="login-form">
+                        <div class="form-group">
+                            <label for="">Email:</label>
+                            <input type="text" name="nome" class="form-control" placeholder="Digite seu email">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Senha:</label>
+                            <input type="Password" name="senha" class="form-control" placeholder="Digite sua senha">
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
